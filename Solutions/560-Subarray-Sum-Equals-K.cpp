dffcbdc4b90f63236689b20289e9151fcf73802b
Prashant Chauhan <psc856@gmail.com>
2025-02-22 12:59:06 +0530

    [560] [Time Beats: 90.64%] [Memory Beats: 67.76%] - LeetPush

--- Solutions/560-Subarray-Sum-Equals-K.cpp
+++ Solutions/560-Subarray-Sum-Equals-K.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
 //brute
+/*
     int subarraySum(vector<int>& nums, int k) {
         int left=0;
         int sum=0,ans=0;
@@ -16,4 +17,24 @@ public:
         }
         return ans;
     }
+    */
+
+
+    //Optimised Approach
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> data;
+        data[0]=1; //0->difference between sum and k if it's 0 then we have got 1! right?
+        int sum=0,ans=0;
+        for(auto x:nums){
+            sum += x;
+            //look for remaining in map;
+            if(data.find(sum-k)!=data.end()){
+                ans+=data[sum-k];
+            }
+            //save in data
+            data[sum]++;
+        }
+        return ans;
+    }
 };
+
